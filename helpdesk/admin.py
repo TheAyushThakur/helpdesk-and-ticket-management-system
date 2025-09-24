3aab2ec34d312e8c790c419e5bcfffa2bf31dc15
TheAyushThakur <ayushthakur6005@gmail.com>
2025-09-24 15:00:38 +0530

    Created a Ticket Model as well and Added both the models in helpdesk/admin.py

--- helpdesk/admin.py
+++ helpdesk/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
+from .models import Profile, Ticket
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display= ('user', 'role')
+    search_fields = ('user__username', 'role')
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display= ('title', 'status', 'priority', 'created_by', 'assigned_to', 'created_at')
+    list_filter = ('priority', 'status')
+    search_fields = ('title', 'priority', 'status', 'assigned_to')
--- helpdesk/models.py
+++ helpdesk/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     Role_Choices=(
         ('user', 'User'),
         ('agent', 'Agent'),
-        ('admin', 'Admin')
+        ('admin', 'Admin'),
     )
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -14,3 +14,30 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+    
+class Ticket(models.Model):
+    Priority_Choices =(
+        ('low', "Low"),
+        ('medium', "Medium"),
+        ('high', "High"),
+    )
+
+    Status_Choices =(
+        ('open', "Open"),
+        ('in-progress', "In Progress"),
+        ('resolved', "Resolved"),
+        ('closed', 'Closed'),
+        ('escalated', 'Escalated'),
+    )
+
+    title = models.CharField(max_length=200)
+    description= models.TextField()
+    priority = models.CharField(max_length=10, choices=Priority_Choices, default="low")
+    status = models.CharField(max_length=20, choices=Status_Choices, default="open")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tickets")
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name='assigned_tickets')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
