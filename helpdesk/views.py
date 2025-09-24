03f5c7a051081c419baa811e3191bde63cc7fc6c
TheAyushThakur <ayushthakur6005@gmail.com>
2025-09-24 16:18:18 +0530

    created a Ticket View set in views.py

--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Ticket
+from .serializers import TicketSerializer
 
 # Create your views here.
+
+class TicketViewSet(viewsets.ModelViewSet):
+    serializer_class = TicketSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'status', 'priority', 'assigned_to']
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Ticket.objects.all()    
+        profile = user.profile
+        if profile.role == 'Agent':
+            return Ticket.objects.filter(assigned_to = user)
+        return Ticket.objects.filter(created_by = user)
+    
+    def perform_create(self, serializer):
+        serializer.save(created_by =self.request.user)
+
+
+        
+
