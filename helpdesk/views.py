b74be4ceca24c96bccafe6641368a2cab4ddbc23
TheAyushThakur <ayushthakur6005@gmail.com>
2025-09-26 13:00:45 +0530

    created authentication Views

--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,10 +1,14 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Ticket
-from .serializers import TicketSerializer
+from .serializers import TicketSerializer, RegisterSerializer, UserSerializer
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
 
+######Ticket######
 class TicketViewSet(viewsets.ModelViewSet):
     serializer_class = TicketSerializer
     queryset = Ticket.objects.all() 
@@ -22,6 +26,37 @@ class TicketViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(created_by =self.request.user)
+
+######Register######
+class RegisterView(generics.CreateAPIView):
+    serializer_class= RegisterSerializer
+    permission_classes= [permissions.AllowAny]
+
+
+######Login######
+class LoginView(APIView):
+    permission_classes= [permissions.AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password= request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return Response({"message": "Login Successful"})
+        else:
+            return Response({"message": "InValid Credentials"}, status=400)
+
+
+######Logout######
+class LogoutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({"message": "Logged out Succesfully"})
+    
+
+
 
 
         
