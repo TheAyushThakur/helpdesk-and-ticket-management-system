702e989e41f37482cd90e1fbf345465814851227
TheAyushThakur <ayushthakur6005@gmail.com>
2025-09-27 00:17:48 +0530

    Added Comments field in Serializers

--- helpdesk/serializers.py
+++ helpdesk/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Ticket, Profile, Comment
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields= ['id', 'username', 'email']
 
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'ticket', 'author', 'comment', 'created_at']
+        read_only_fields = ['author', 'created_at']
+
 class TicketSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
     created_by = UserSerializer(read_only= True)
     assigned_to = UserSerializer(read_only = True)
 
@@ -33,10 +41,3 @@ class LoginSerializer(serializers.Serializer):
     username =serializers.CharField()
     password= serializers.CharField(write_only=True)
 
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
-
-    class Meta:
-        model = Comment
-        fields = ['id', 'ticket', 'author', 'text', 'created_at']
-        read_only_fields = ['author', 'created_at']
