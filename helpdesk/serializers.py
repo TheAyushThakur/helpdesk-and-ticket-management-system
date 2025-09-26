d43fa4fa90d7f573a259ad4c3eb76536532b38c5
TheAyushThakur <ayushthakur6005@gmail.com>
2025-09-26 14:28:05 +0530

    Updated Login View and Serializer to show Username and Password fields

--- helpdesk/serializers.py
+++ helpdesk/serializers.py
@@ -29,6 +29,6 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         return user
 
-class LoginSerializer(serializers.ModelSerializer):
+class LoginSerializer(serializers.Serializer):
     username =serializers.CharField()
     password= serializers.CharField(write_only=True)
--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets, permissions, generics
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Ticket
-from .serializers import TicketSerializer, RegisterSerializer, UserSerializer
+from .serializers import TicketSerializer, RegisterSerializer, UserSerializer, LoginSerializer
 from django.contrib.auth import authenticate, login, logout
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
 
 # Create your views here.
 
@@ -35,19 +36,25 @@ class RegisterView(generics.CreateAPIView):
 
 ######Login######
 class LoginView(APIView):
-    permission_classes= [permissions.AllowAny]
+    permission_classes = [permissions.AllowAny]
+    
+    @swagger_auto_schema(
+            request_body= LoginSerializer,
+            responses={200: "Login Successful", 400: "InValid Credentials"}
+    )
 
     def post(self, request):
-        username = request.data.get('username')
-        password= request.data.get('password')
-        user = authenticate(request, username=username, password=password)
+        serializer= LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
+        username = serializer.validated_data["username"]
+        password = serializer.validated_data["password"]
+
+        user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return Response({"message": "Login Successful"})
-        else:
-            return Response({"message": "InValid Credentials"}, status=400)
-
+            return Response({"message": "Login successful"})
+        return Response({"error": "Invalid credentials"}, status=400)
 
 ######Logout######
 class LogoutView(APIView):
