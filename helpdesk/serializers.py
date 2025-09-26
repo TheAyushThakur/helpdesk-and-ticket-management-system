32dedb76ba0b576c2c500eb4b1378afef5414c0f
TheAyushThakur <ayushthakur6005@gmail.com>
2025-09-26 12:47:25 +0530

    Created a registration serializer

--- helpdesk/serializers.py
+++ helpdesk/serializers.py
@@ -14,3 +14,17 @@ class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = '__all__'
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password= serializers.CharField(write_only= True)
+    class meta:
+        model = User
+        fields= ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username= validated_data['username'],
+            email= validated_data.get('email'),
+            password= validated_data['password']
+        )
+        return user
